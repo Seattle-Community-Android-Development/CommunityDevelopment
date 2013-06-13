6e6f3df85e3da587e28f9c9857328391e9c93c64
Don <helo478@gmail.com>
2013-06-13 13:59:24 -0700

    Adding Fragments for Time and Date pickers

---
+++ Groupproject/Groupproject/src/main/java/com/example/groupproject/DatePickerFragment.java
@@ -0,0 +1,33 @@
+package com.example.groupproject;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+
+/**
+ * Created by Donald on 6/4/13.
+ */
+public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        String string = month + "-" + day + " " + year;
+        AddEventFragment fragment = (AddEventFragment) getTargetFragment();
+        fragment.setDate(string);
+    }
+}
---
+++ Groupproject/Groupproject/src/main/java/com/example/groupproject/TimePickerFragment.java
@@ -0,0 +1,34 @@
+package com.example.groupproject;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+/**
+ * Created by Donald and Todd on 6/4/13.
+ */
+public class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        return new TimePickerDialog(getActivity(), this, hour, minute, DateFormat.is24HourFormat(getActivity()));
+    }
+
+
+    @Override
+    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+        String string = hourOfDay + ":" + minute;
+        AddEventFragment fragment = (AddEventFragment) getTargetFragment();
+        fragment.setTime(string);
+    }
+}
