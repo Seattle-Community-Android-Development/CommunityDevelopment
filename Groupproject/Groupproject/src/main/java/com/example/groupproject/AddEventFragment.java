78baa50d8aa1418b33bd670a6c3e896508e921ec
Don <helo478@gmail.com>
2013-06-13 14:04:00 -0700

    Updating AddEventFragment.java
    This update works with the new Time and Date picker fragments

--- Groupproject/Groupproject/src/main/java/com/example/groupproject/AddEventFragment.java
+++ Groupproject/Groupproject/src/main/java/com/example/groupproject/AddEventFragment.java
@@ -1,24 +1,53 @@
 package com.example.groupproject;
 
+import android.app.Activity;
 import android.app.DialogFragment;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 
 import java.util.Calendar;
 
 /**
- * Created by toddfrisch on 5/31/13.
+ * Created by toddfrisch and Donald Subert on 5/31/13.
  */
 public class AddEventFragment extends Fragment
 {
+    private final String TAG = getClass().getSimpleName();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        setUpEventListening();
+    }
+
+    private void setUpEventListening() {
+
+        Button timeButton = (Button) getActivity().findViewById(R.id.editText_event_time);
+        timeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTimePickerDialog();
+            }
+        });
+
+        Button dateButton = (Button) getActivity().findViewById(R.id.editText_event_date);
+        dateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog();
+            }
+        });
     }
 
     @Override
@@ -27,7 +56,39 @@ public class AddEventFragment extends Fragment
         return inflater.inflate(R.layout.add_event_fragment, container, false);
     }
 
-//-----------------------------------------this does not include the date/time or address for simplicity to see if the application works as expected through this phase. should probably go back after execution of this method.
+    public void showTimePickerDialog() {
+
+        if (ProjectApplication.L) {
+            Log.d(TAG, "showTimePickerDialog()");
+        }
+
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.setTargetFragment(this, 0);
+        newFragment.show(getFragmentManager(), "timePicker");
+    }
+
+    public void showDatePickerDialog() {
+
+        if (ProjectApplication.L) {
+            Log.d(TAG, "showDatePickerDialog()");
+        }
+
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.setTargetFragment(this, 0);
+        newFragment.show(getFragmentManager(), "datePicker");
+    }
+
+    public void setTime(String time) {
+        Button timeButton = (Button) getActivity().findViewById(R.id.editText_event_time);
+        timeButton.setText(time);
+    }
+
+    public void setDate(String date) {
+        Button dateButton = (Button) getActivity().findViewById(R.id.editText_event_date);
+        dateButton.setText(date);
+    }
+
+    //-----------------------------------------this does not include the date/time or address for simplicity to see if the application works as expected through this phase. should probably go back after execution of this method.
 //should be shifted to an AsyncTask
     public void onClick(View view){
 
@@ -49,7 +110,7 @@ public class AddEventFragment extends Fragment
         //event.se
 
 
-                ((ProjectApplication) getActivity().getApplication()).eventData.insert(event);
+        ((ProjectApplication) getActivity().getApplication()).eventData.insert(event);
 
 
 
